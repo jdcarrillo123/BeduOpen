424b16f0c68886d07a3dc6b08900635b6b1899e3
jdcarrillo123 <dcarrillo@idea.gob.mx>
2022-05-24 12:27:04 -0500

    add operadores js

---
+++ js/practica01/operadores.js
@@ -0,0 +1,34 @@
+//operadores aritmeticos permiten realizar operaciones matematicas
+let suma=2+3;
+console.log("Operadores aritmeticos");
+console.log("Suma "+suma);
+let resta=2-3;
+console.log("Resta "+resta);
+let multiplicacion=2*3;
+console.log("Multiplicación "+multiplicacion);
+let division=2/3;
+console.log("Division "+division);
+let exponente=2**3;
+console.log("Exponienciacion "+exponente);
+let residuo=2%3;
+console.log("Residuo o modulo " +residuo);
+//operadores de autoincremento
+let x=0;
+console.log("Valor inicial de x=0");
+x++;
+console.log("Aumenta el valor de x en uno: "+x);
+x--;
+console.log("Disminuye el valor en uno "+x);
+++x;
+console.log("Aumenta el valor de x primero se hace el cambio y luego asigna la variable "+ x);
+--x;
+console.log("Disminuye el valor en uno x primero hace el cambio "+x);
+//Operadores de comparacion
+x=1;
+let y=3;
+console.log("El operador de igualdad x==y, el valor de x es:="+x+" El valor de y="+y+" ¿son iguales?",x==y);
+console.log("El operador de desigualdad x!=y, el valor de x es:="+x+" El valor de y="+y+" ¿son iguales?",x!=y);
+console.log("El operador mayor que x>y, el valor de x es:="+x+" El valor de y="+y+" ¿el valor de x es mayor que el de y?",x>y);
+console.log("El operador menor que x<y, el valor de x es:="+x+" El valor de y="+y+" ¿el valor de x es menor que el de y?",x<y);
+console.log("El operador mayor igual que x>=y, el valor de x es:="+x+" El valor de y="+y+" ¿el valor de x es mayor o igual que el de y?",x>=y);
+console.log("El operador menor igual que x<=y, el valor de x es:="+x+" El valor de y="+y+" ¿el valor de x es menor que el de y?",x<=y);
---
+++ js/practica01/variables.js
@@ -0,0 +1,13 @@
+var nombre="Juan Daniel"; //se utiliza la palabra var para declarar una variable se puede declarar sin inicializarla si la variable no se inicializa agrega undefine
+console.log(nombre);
+nombre="otro nombre";
+console.log(nombre);
+var nombre=25;
+console.log(nombre);
+let apellido; //se utiliza para declarar variables y al igual que var se puede declarar sin inicializar pero let no asigna el valor undefined
+//let no permite redefinir la variable como en el caso de var
+console.log(apellido);
+apellido="Carrillo";
+//otra opcion para definir variables es tambien para declarar variables, const no sirve para declarar variables
+const materno="Mercado"; //const siempre hay que inicializar la variable, y al igual que let no permite redefinir
+
