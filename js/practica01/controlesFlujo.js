ed8b5b76cd6e30036a735937ad07179776e91cd5
jdcarrillo123 <dcarrillo@idea.gob.mx>
2022-05-24 13:46:00 -0500

    add controlesFlujo.js

---
+++ js/practica01/controlesFlujo.js
@@ -0,0 +1,19 @@
+//Controles de flujo
+console.log("Controles de flujo, o cliclos");
+//while
+let x=10;
+console.log("El ciclo se repetira mientras x>0, El ciclo while se utiliza cuando no sabemos cuantas veces se va a repetir el codigo ")
+while (x > 0){ //guardia de la condicion
+    x-=1;
+    console.log("el valor de x = ",x);
+
+}
+console.log("El ciclo terminó porque ya no se cumplio la condicion de la guardia")
+//for 
+
+console.log("Se usa el Ciclo for cuando sabemos cuantas veces se va a repetir el codigo");
+for(x =10; x>0; x-=1){
+    console.log("el valor de x = ",x);
+}
+//ciclo infinito
+
