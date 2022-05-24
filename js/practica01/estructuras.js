1692f0e3b75cbda7bf068ab18fae885b7921be79
jdcarrillo123 <dcarrillo@idea.gob.mx>
2022-05-24 15:00:09 -0500

    add estructuras.js

---
+++ js/practica01/estructuras.js
@@ -0,0 +1,10 @@
+//objetos, arreglos y funciones
+console.log("Objetos, arreglos y funciones");
+console.log("AGGREGLOS");
+let numbers=[5,3,4,7,2,1,9,7];
+let i;
+let suma=0;
+for(i=0; i<numbers.length; i+=1){
+    suma=suma+numbers[i];
+}
+console.log(suma/numbers.length);
