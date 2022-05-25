8c9ba771a0fda2f400d6cbff6645382a19448047
jdcarrillo123 <dcarrillo@idea.gob.mx>
2022-05-25 13:36:13 -0500

    Arreglo en key, value pasado a objeto

--- js/practica01/estructuras.js
+++ js/practica01/estructuras.js
@@ -8,3 +8,19 @@ for(i=0; i<numbers.length; i+=1){
     suma=suma+numbers[i];
 }
 console.log(suma/numbers.length);
+ //Objetos
+ console.log("Crear objeto con laos valores que lleva un objeto con pares [key,value]");
+ let car=[["brand","Nissan"],["model","Versa"],["year",2022]];
+ var arCar=[];
+ var obj={};
+ for(let i=0;i<car.length;i+=1){
+    arCar=car[i]
+    //console.log(car[i]);
+    for(let j=0;j<arCar.length;j+=1){
+        //console.log(arCar[j]);
+        Object.defineProperty(obj,arCar[0],{value:arCar[1],writable:true,enumerable:true,configurable:true}); 
+    }
+ }
+ console.log(obj);
+ 
+ 
